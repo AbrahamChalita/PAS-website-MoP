5e69d1698a1068727604d3cf82461f5ed680048e
Eduardo AG <edag4000@gmail.com>
2022-03-29 20:43:51 -0600

    Agregando GET message para obtener datos de resultados de preguntas por id de usuario

--- MoP/apiServices/urls.py
+++ MoP/apiServices/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('user_info', views.user_info, name='user_info'),
+	path('quiz_info', views.quiz_info, name='quiz_info'),
 ]
--- MoP/apiServices/views.py
+++ MoP/apiServices/views.py
@@ -33,3 +33,51 @@ def user_info(request):
 		lista_salida.append(d)
 		j = dumps(lista_salida)
 		return HttpResponse(j, content_type="text/json-comment-filtered")
+
+# quiz_info GET message
+# Gives a json file with UserID, Username, HashPwd, Country & Name of a user
+# Needs following format  =>  ?UserID=X
+def quiz_info(request):
+	query = request.GET['UserID']
+	mydb = sqlite3.connect("db.sqlite3")
+	cur = mydb.cursor()
+	stringSQL = '''
+	SELECT QuestionID, QuestionTxt, TimesFailed, TimesCorrect
+	FROM Question 
+	WHERE QuestionID IN
+	(
+		SELECT QuestionID 
+		FROM QuestionGame 
+		WHERE GameID =
+		(
+			SELECT GameID 
+			FROM GameResume 
+			WHERE UserID = ?
+		)
+	)'''
+	rows = cur.execute(stringSQL,(query,))
+	r = rows.fetchone()
+		
+	lista_salida = []
+
+	if r == None:
+		raise Http404("User does not exist")
+	else:
+		# Agregando primer resultado de querry
+		d = {}
+		d["QuestionID"] = r[0]
+		d["QuestionTxt"] = r[1]
+		d["TimesFailed"] = r[2]	
+		d["TimesCorrect"] = r[3]
+		lista_salida.append(d)
+		# Agregando demas resultados de quesrry si existen
+		for r in rows:
+			d = {}
+			d["QuestionID"] = r[0]
+			d["QuestionTxt"] = r[1]
+			d["TimesFailed"] = r[2]	
+			d["TimesCorrect"] = r[3]
+			lista_salida.append(d)
+			
+		j = dumps(lista_salida)
+		return HttpResponse(j, content_type="text/json-comment-filtered")
