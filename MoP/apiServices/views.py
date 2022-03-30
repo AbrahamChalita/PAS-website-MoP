046e175e7b435ae81444da50c74fbe9fbd112bd4
Eduardo AG <edag4000@gmail.com>
2022-03-29 20:05:35 -0600

    Agregando display dummy de API. Comprobando conexion y agregando tablas a DB.

--- MoP/apiServices/views.py
+++ MoP/apiServices/views.py
@@ -1,3 +1,9 @@
+import sqlite3
+from django.http import HttpResponse, Http404, JsonResponse
+from django.template import loader
 from django.shortcuts import render
+from json import dumps, load, loads
 
 # Create your views here.
+def index(request):
+	return HttpResponse("<h1>Hola desde Django!</h1>")
