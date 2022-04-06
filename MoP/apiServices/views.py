a6bcb91f159c5df6ad785c7de108688863b8148e
Abraham Chalita <abrahamchalita@hotmail.com>
2022-04-06 13:59:16 -0500

    Deleted views funtions service apis for general stats

--- MoP/apiServices/views.py
+++ MoP/apiServices/views.py
@@ -9,61 +9,14 @@ from json import dumps, load, loads
 def main(request):
 	return render(request, 'main.html')
 
-# Gives json file with overall player count and overall playedtime api/totalplayers_stats
-def totalplayers_stats(request):
-	mydb = sqlite3.connect("db.sqlite3")
-	curr = mydb.cursor()
-	stringSQL = '''SELECT (
-		SELECT COUNT (DISTINCT UserID) from GameResume) 
-		as total_players, 
-		(SELECT SUM(Seconds) as total_playtime from GameResume) 
-		as total_playtime
-	'''
-	rows = curr.execute(stringSQL)
-	listaSalida = []
-
-	for r in rows:
-		d = {}
-		d['TotalPlayers'] = r[0]
-		d['TotalPlayTime'] = r[1]
-		listaSalida.append(d)
-	
-	j = dumps(listaSalida)
-	return HttpResponse(j, content_type="text/json-comment-filtered")
-
-# Gives json file with information to construct leaderboard api/leaderboard_stats
-def leaderboard_stats(request):
-	mydb = sqlite3.connect("db.sqlite3")
-	curr = mydb.cursor()
-	stringSQL ='''SELECT QuizGame.QuizID, QuizGame.Score, GameResume.UserID, 
-		(SELECT country from User where UserID = GameResume.UserID),
-		(SELECT username from User where UserID = GameResume.UserID) from QuizGame
-		INNER JOIN GameResume on QuizGame.GameID = GameResume.GameID 
-		ORDER by Score DESC
-	'''
-	rows = curr.execute(stringSQL)
-	listaSalida = []
-
-	for r in rows:
-		d = {}
-		d['QuizId'] = r[0]
-		d['Score'] = r[1]
-		d['UserId'] = r[2]
-		d['Username'] = r[3]
-		d['Country'] = r[4]
-		listaSalida.append(d)
-	
-	j = dumps(listaSalida)
-	return HttpResponse(j, content_type="text/json-comment-filtered")
-
 # Gives json file most used instrument api/instrument_stats
 def instrument_stats(request):
 	mydb = sqlite3.connect("db.sqlite3")
 	curr = mydb.cursor()
 	stringSQL ='''SELECT  UserInstrument.DmgDone, UserInstrument.Playtime, Instrument.Description,
-		(SELECT username from User where UserID = UserInstrument.UserID) 
+		(SELECT username from User where UserID = UserInstrument.UserID)
 		from Instrument
-		INNER JOIN UserInstrument on UserInstrument.InstrumentID = Instrument.InstrumentID 
+		INNER JOIN UserInstrument on UserInstrument.InstrumentID = Instrument.InstrumentID
 		ORDER BY DmgDone DESC
 	'''
 	rows = curr.execute(stringSQL)
@@ -76,6 +29,6 @@ def instrument_stats(request):
 		d['Description'] = r[2]
 		d['Username'] = r[3]
 		listaSalida.append(d)
-	
+
 	j = dumps(listaSalida)
 	return HttpResponse(j, content_type="text/json-comment-filtered")
