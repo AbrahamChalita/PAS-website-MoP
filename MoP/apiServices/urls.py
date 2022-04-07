d57ee80c2f5b32d20297554c11468297d4e2eedc
Abraham <abrahamchalita@hotmail.com>
2022-04-07 11:35:09 -0500

    deleted some url paths

--- MoP/apiServices/urls.py
+++ MoP/apiServices/urls.py
@@ -6,7 +6,5 @@ from . import views
 
 urlpatterns = [
 	path('', views.main, name='main'),
-	path('totalplayers_stats', views.totalplayers_stats, name='totalplayers_stats'),
-	path('leaderboard_stats', views.leaderboard_stats, name='leaderboard_stats'),
 	path('instrument_stats', views.instrument_stats, name='instrument_stats'),
 ]
