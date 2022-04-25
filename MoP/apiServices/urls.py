aa057e1a1d18e6e82f6ca0fb34b7996862769b64
Eduardo AG <edag4000@gmail.com>
2022-04-25 18:07:04 -0500

    Modificando orden de urls e informacion de la base de datos

--- MoP/apiServices/urls.py
+++ MoP/apiServices/urls.py
@@ -6,22 +6,27 @@ from . import views
 
 urlpatterns = [
 	path('', views.main, name='main'),
-	path('progress_info', views.progress_info, name='progress_info'),
-    path('unity2', views.unity2, name='unity2'),
-    path('unity3', views.unity3, name='unity3'),
 	path('user_info', views.user_info, name='user_info'),
+
+    path('unity2', views.unity2, name='unity2'), # Login
+    path('unity3', views.unity3, name='unity3'), # Registro
+	path('change_userName', views.change_userName, name='change_userName'), # Cambiar nombre de usuario
+	path('new_game', views.new_game, name='new_game'), # Crea nueva partida
+	path('get_games', views.get_games, name='get_games'), # Partidas del usuario
+	path('update_checkpoint', views.update_checkpoint, name='update_checkpoint'), # Actualiza checkpoint al entrar a un hubworld
+
+	path('save_level', views.save_level, name='save_level'), # Guarda datos de nivel de plataforma
+	
+	path('quiz_request', views.quiz_request, name='quiz_request'), # Recupera preguntas y respuestas
+	path('save_quiz', views.save_quiz, name='save_quiz'), # Guarda datos del quiz
+
+	path('quizes_played', views.quizes_played, name='quizes_played'), # Quizes jugados por el usuario
+	path('score_by_quiz', views.score_by_quiz, name='score_by_quiz'), # Scores del jugador por quiz
+
+	path('progress_info', views.progress_info, name='progress_info'),
 	path('quiz_info', views.quiz_info, name='quiz_info'),
-	path('get_games', views.get_games, name='get_games'),
 	path('get_quiz_data', views.get_quiz_data, name='get_quiz_data'),
-	path('quiz_request', views.quiz_request, name='quiz_request'),
-	path('save_quiz', views.save_quiz, name='save_quiz'),
-	path('save_level', views.save_level, name='save_level'),
-	path('change_userName', views.change_userName, name='change_userName'),
-	path('new_game', views.new_game, name='new_game'),
 	path('instrument_stats', views.instrument_stats, name='instrument_stats'),
 	path('GameResumeList', views.GameResumeList, name='GameResumeList'),
 	path('GameResumesUser', views.GameResumesUser, name='GameResumesUser'),
-	path('quizes_played', views.quizes_played, name='quizes_played'),
-	path('score_by_quiz', views.score_by_quiz, name='score_by_quiz'),
-	path('update_checkpoint', views.update_checkpoint, name='update_checkpoint'),
 ]
